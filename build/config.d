a7745bf404d8900ae81a87ffa85a8652a02405c0
naga <38852591+NaGaa95@users.noreply.github.com>
2026-06-30 09:52:49 +0200

    Revert "switch port"
    This reverts commit 31f1912ecaaabb71a4335fc0b0f712692a0bb15f.

--- build/config.d
+++
@@ -1,5 +0,0 @@
-config.o: C:/Users/Zenbook/Documents/GitHub/aom_nx/source/config.c \
- C:/Users/Zenbook/Documents/GitHub/aom_nx/source/config.h \
- C:/Users/Zenbook/Documents/GitHub/aom_nx/source/util.h
-C:/Users/Zenbook/Documents/GitHub/aom_nx/source/config.h:
-C:/Users/Zenbook/Documents/GitHub/aom_nx/source/util.h:
